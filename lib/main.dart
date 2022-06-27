c290766105e92c4b5d82db24e6b9f28f7b4bc36e
Emmanuel Peter Amexo <peteremmanuel0544@gmail.com>
2022-06-27 13:23:53 +0000

    changes to text styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +13,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         // appBar: AppBar(),
-        body: Center(
-          child: Column(children: [
-            Image.asset("assets/images/logo.png"),
-            const Text("Accra International School"),
-            const Text("Training our leaders for today")
-          ]),
-        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/logo.png",
+                width: 214.25,
+                height: 162,
+              ),
+              const Text("Accra International School",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  )),
+              // const Text("Accra International School",
+              //     style: GoogleFonts.oswald(textStyle: display1),),
+              const Text(
+                "Training our leaders for today",
+                style: TextStyle(fontSize: 16),
+              )
+            ]),
       ),
     );
   }
