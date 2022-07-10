e2a5ffde65705a50c27486b17003dab039d02126
Emmanuel Peter Amexo <peteremmanuel0544@gmail.com>
2022-07-10 00:26:30 +0000

    notifications page editing

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -6,34 +6,37 @@ class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/notifications.png',
-            width: 300.55,
-            height: 300,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              'No notifications yet!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/notifications.png',
+              width: 300.55,
+              height: 300,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 2),
-            child: Text(
-              'Retry later to check for any new notifications',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: Color.fromRGBO(0, 0, 0, 0.7),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'No notifications yet!',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
               ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.only(top: 2),
+              child: Text(
+                'Retry later to check for any new notifications',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromRGBO(0, 0, 0, 0.7),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
