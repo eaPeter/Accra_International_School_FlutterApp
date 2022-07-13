885c00e4bfb0ea205e6cc267d89fbe6b2e04d6e0
Emmanuel Peter Amexo <peteremmanuel0544@gmail.com>
2022-07-13 16:09:32 +0000

    updatind drawer

--- lib/navigation_drawer_widget.dart
+++ lib/navigation_drawer_widget.dart
@@ -7,34 +7,50 @@ class NavigationDrawerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
         child: Material(
-      child: ListView(
-        children: [
-          const SizedBox(height: 10),
-          buildMenuItem(
-            icon: Icons.favorite_border_outlined,
-            text: 'My Courses',
-          ),
-          const SizedBox(height: 10),
-          buildMenuItem(
-            icon: Icons.download_outlined,
-            text: 'Downloads',
-          ),
-          const SizedBox(height: 10),
-          buildMenuItem(
-            icon: Icons.favorite_border_outlined,
-            text: 'Assignments',
-          ),
-          const SizedBox(height: 10),
-          buildMenuItem(
-            icon: Icons.timelapse_outlined,
-            text: 'Deadlines',
-          ),
-          const SizedBox(height: 10),
-          buildMenuItem(
-            icon: Icons.settings_outlined,
-            text: 'Settings',
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.favorite_border_outlined,
+              text: 'My Courses',
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.download_outlined,
+              text: 'Downloads',
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.favorite_border_outlined,
+              text: 'Assignments',
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.timelapse_outlined,
+              text: 'Deadlines',
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.settings_outlined,
+              text: 'Settings',
+            ),
+            const Divider(
+              thickness: 2.0,
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.info_outline,
+              text: 'Info',
+            ),
+            const SizedBox(height: 10),
+            buildMenuItem(
+              icon: Icons.logout_outlined,
+              text: 'Log out',
+            ),
+          ],
+        ),
       ),
     ));
   }
