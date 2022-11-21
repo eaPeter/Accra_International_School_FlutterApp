4931e8384577db3b0a3ef3d1b015f801e4c55db0
eaPeter <peteremmanuel0544@gmail.com>
2022-11-21 17:21:12 +0000

    Update see_All.dart

--- lib/see_All.dart
+++ lib/see_All.dart
@@ -3,26 +3,7 @@ import 'package:accra_app/home.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class SeeAll extends StatelessWidget {
-  SeeAll({Key? key}) : super(key: key);
-
-  List<CourseList> courseList = [
-    const CourseList(
-        courseImg: 'english1.png',
-        title: 'The Good English Academy Aljamar',
-        subtitle: 'Junior High Edition'),
-    const CourseList(
-        courseImg: 'english2.png',
-        title: 'The Good English Academy Aljamar',
-        subtitle: 'Junior High Edition'),
-    const CourseList(
-        courseImg: 'english1.png',
-        title: 'The Good English Academy Aljamar',
-        subtitle: 'Junior High Edition'),
-    const CourseList(
-        courseImg: 'english2.png',
-        title: 'The Good English Academy Aljamar',
-        subtitle: 'Junior High Edition'),
-  ];
+  const SeeAll({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -49,19 +30,161 @@ class SeeAll extends StatelessWidget {
         // ),
         backgroundColor: Colors.white,
       ),
-      body: ListView.builder(
-          itemCount: courseList.length,
-          itemBuilder: (context, index) {
-            final course = courseList[index];
-            return Card(
-              child: ListTile(
-                title: Text(course.title),
-                subtitle: Text(course.subtitle),
-                // courseImg: Image.asset(course.courseImg),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+                width: double.infinity,
               ),
-            );
-          }),
+              Row(
+                children: const [
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                  Spacer(),
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+                width: double.infinity,
+              ),
+              Row(
+                children: const [
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                  Spacer(),
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+                width: double.infinity,
+              ),
+              Row(
+                children: const [
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                  Spacer(),
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+                width: double.infinity,
+              ),
+              Row(
+                children: const [
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                  Spacer(),
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+                width: double.infinity,
+              ),
+              Row(
+                children: const [
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                  Spacer(),
+                  SeeAllCourses(
+                      image: "english1.png",
+                      title: "The Good English Academy Aljamar",
+                      subtitle: "Junior High Edition"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class SeeAllCourses extends StatelessWidget {
+  const SeeAllCourses(
+      {Key? key,
+      required this.image,
+      required this.title,
+      required this.subtitle})
+      : super(key: key);
+  final String image;
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/$image",
+            width: 174,
+            height: 125,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: SizedBox(
+              width: 174,
+              child: Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Text(
+              subtitle,
+              style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.45)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 0),
+            child: Row(
+              children: const [
+                Text(
+                  "5.0",
+                  style: TextStyle(fontSize: 12),
+                ),
+                Ratings(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ]);
   }
 }
 
@@ -75,228 +198,3 @@ class CourseList {
   final String title;
   final String subtitle;
 }
-
-// class Courses extends StatelessWidget {
-//   const Courses(
-//       {Key? key,
-//       required this.image,
-//       required this.title,
-//       required this.subtitle})
-//       : super(key: key);
-//   final String image;
-//   final String title;
-//   final String subtitle;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(children: [
-//       const SizedBox(
-//         height: 15,
-//         width: double.infinity,
-//       ),
-//       Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Image.asset(
-//             "assets/images/$image",
-//             width: 200,
-//             height: 125,
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 15),
-//             child: SizedBox(
-//               width: 200,
-//               child: Text(
-//                 title,
-//                 style:
-//                     const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 5),
-//             child: Text(
-//               subtitle,
-//               style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.45)),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 0),
-//             child: Row(
-//               children: const [
-//                 Text(
-//                   "5.0",
-//                   style: TextStyle(fontSize: 12),
-//                 ),
-//                 Ratings(),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     ]);
-//   }
-// }
-
-// class EnglishCourse extends StatelessWidget {
-//   const EnglishCourse({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.separated(
-//       itemCount: 20,
-//       separatorBuilder: (context, index) {
-//         return const SizedBox(
-//           height: 24,
-//         );
-//       },
-//       itemBuilder: (context, index) {
-//         return const EnglishCourseAll();
-//         // return buildCard(index);
-//       },
-
-//       // child: Column(
-//       //   children: [
-//       //     CarouselSlider(
-//       //       options: CarouselOptions(
-//       //           height: 250.0, autoPlay: true, viewportFraction: 0.6),
-//       //       items: [1, 2, 3, 4, 5].map((i) {
-//       //         return Builder(
-//       //           builder: (BuildContext context) {
-//       //             return SizedBox(
-//       //               width: MediaQuery.of(context).size.width,
-//       //               child: Column(
-//       //                 children: const [
-//       //                   Courses(
-//       //                       image: "english1.png",
-//       //                       title: "The Good English Academy Aljamar",
-//       //                       subtitle: "Junior High Edition"),
-//       //                   // Courses(
-//       //                   //     image: "english2.png",
-//       //                   //     title: "The Good English Academy Aljamar",
-//       //                   //     subtitle: "Junior High Edition"),
-//       //                 ],
-//       //               ),
-//       //             );
-//       //           },
-//       //         );
-//       //       }).toList(),
-//       //     ),
-//       //   ],
-//       // ),
-//     );
-//   }
-
-//   Widget buildCard(int index) => SizedBox(
-//         child: Row(
-//           children: const [
-//             Courses(
-//                 image: "english1.png",
-//                 title: "The Good English Academy Aljamar",
-//                 subtitle: "Junior High Edition"),
-//             Courses(
-//                 image: "english2.png",
-//                 title: "The Good English Academy Aljamar",
-//                 subtitle: "Junior High Edition"),
-//           ],
-//         ),
-//       );
-// }
-
-// class CoursesAll extends StatelessWidget {
-//   const CoursesAll(
-//       {Key? key,
-//       required this.image,
-//       required this.title,
-//       required this.subtitle})
-//       : super(key: key);
-//   final String image;
-//   final String title;
-//   final String subtitle;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(children: [
-//       const SizedBox(
-//         height: 15,
-//         width: double.infinity,
-//       ),
-//       Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Image.asset(
-//             "assets/images/$image",
-//             width: 200,
-//             height: 125,
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 15),
-//             child: SizedBox(
-//               width: 200,
-//               child: Text(
-//                 title,
-//                 style:
-//                     const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 5),
-//             child: Text(
-//               subtitle,
-//               style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.45)),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(top: 0),
-//             child: Row(
-//               children: const [
-//                 Text(
-//                   "5.0",
-//                   style: TextStyle(fontSize: 12),
-//                 ),
-//                 Ratings(),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     ]);
-//   }
-// }
-
-// class EnglishCourseAll extends StatelessWidget {
-//   const EnglishCourseAll({Key? key}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         CarouselSlider(
-//           options: CarouselOptions(
-//               height: 250.0, autoPlay: true, viewportFraction: 0.6),
-//           items: [1, 2, 3, 4, 5].map((i) {
-//             return Builder(
-//               builder: (BuildContext context) {
-//                 return SizedBox(
-//                   width: MediaQuery.of(context).size.width,
-//                   child: Column(
-//                     children: const [
-//                       Courses(
-//                           image: "english1.png",
-//                           title: "The Good English Academy Aljamar",
-//                           subtitle: "Junior High Edition"),
-//                     ],
-//                   ),
-//                 );
-//               },
-//             );
-//           }).toList(),
-//         ),
-//       ],
-//     );
-//   }
-// }
